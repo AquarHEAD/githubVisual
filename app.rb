0b310da6f80447f83ab166263f565d442e9aa053
AquarHEAD Lou <aquarhead@gmail.com>
2014-01-08 01:14:41 +0800

    finish language view.

--- app.rb
+++ app.rb
@@ -32,8 +32,14 @@ end
 get '/2013/:month/lang/:lang/?' do
   @days = [31,28,31,30,31,30,31,31,30,31,30,31]
   @month = params[:month].to_i
+  @lang = params[:lang]
   redis = Redis.new(port:7755)
-  @hours = (redis.lrange "2013-#{@month}_hours", 0, -1).to_json
+  kk = []
+  (0..23).each do |hh|
+    kk.push (redis.hget "2013-#{@month}_hour_#{hh}", @lang).to_i
+  end
+  @hmax = kk.max
+  @hours = (kk).to_json
   mlangs = ((redis.hgetall "2013-#{@month}").sort_by { |k, v| v.to_i}).reverse
   @langs = (mlangs.map { |k| [k[0], k[1].to_i] }).to_json
   @langscolor = @@langscolor.to_json
